c0833435d7536afe9ecbb949fe26ee345dfb2be8
waitlangpar <germodenmhar@gmail.com>
2025-09-26 22:55:26 +0800

    .

--- Frontend/app/(tabs)/index.tsx
+++ Frontend/app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Index() {
     return (
         <View>
-            <Text>HAHAHAHAHAHAHAHAHA</Text>
+                <Text>HAHAHAHAHAHAHAHHA</Text>
         </View>
     )
 }
